4a8db486926fd521e82ca157484f807479eec4cc
codecraftwt <codecraftwt@gmail.com>
2024-11-18 15:59:33 +0530

    resolved error

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ app.use(cors(corsOptions));
 app.use(bodyParser.json())
 // app.use(bodyParser.urlencoded({ extended: true }))
 
-app.use((req, res, next) => {ss
+app.use((req, res, next) => {
     req.io = io;
     next();
 });
